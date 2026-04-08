5b2f0fef995598b3604ffe1624bf47e74f2f4742
amanda lacerda <amanda.oliveira.lacerda@escola.pr.gov.br>
2026-04-08 13:01:26 +0000

    3

--- classeAutoHeranca.js
+++ classeAutoHeranca.js
@@ -10,11 +10,15 @@ class Automovel{
     }
 }
  class Carro extends Automovel{
-    constructor(marca, modelo, ano, portas){
+    constructor(marca, modelo, ano, cilindrada){
         super(marca, modelo, ano);
-         this.portas = portas;
+         this.cilindrada = cilindrada;
     }
     exibirDetalhes(){
-        return ` ${super.exibirDetalhes()}, Portas: ${this.portas};`
+        return ` ${super.exibirDetalhes()}, cinlindrada: ${this.cilindrada};`
     } 
 }
+let moto1 = new Moto("Honda","CBX 250 - TWISTER", 2007, 249);
+console.log(moto1.exibirDetalhes());
+let carro1 = new Carro("voyage", "Volkswagen 1.6", 2013, 5);
+console.log(carro1.exibirDetalhes());
